cd534ce6850a1afa999f21554bb0650f17ef5214
Karl Eerik <karlvaidl@gmail.com>
2023-12-19 16:17:14 +0200

    Lisasin mitu taustavalikut ning nüüd peaks olema mängul kõik vajalikud funktsioonid olemas

--- Beetaversioon.py
+++ Beetaversioon.py
@@ -187,7 +187,6 @@ class Coin(pygame.sprite.Sprite):
         self.destroy()
 
 
-
 SCREEN_WIDTH = 1100
 SCREEN_HEIGHT = 800
 FPS = 60
@@ -236,23 +235,6 @@ obstacle_group = pygame.sprite.Group() #Kuna mitu yksust, siis vaja Group ning e
 coinsgrp = pygame.sprite.GroupSingle()
 coinsgrp.add(Coin())
 
-
-
-
-
-#Tausta pildi info
-background1 = pygame.image.load('pildid/taust/background1.png')
-background1 = pygame.transform.scale(background1,(1000,800))
-background2 = pygame.image.load('pildid/taust/background2.png')
-background2 = pygame.transform.scale(background2,(1000,800))
-background3 = pygame.image.load('pildid/taust/background3.png')
-background3 = pygame.transform.scale(background3,(1000,800))
- 
- 
-bg1_width = background1.get_width()
-bg2_width = background2.get_width()
-bg3_width = background3.get_width()
-
 #Font info
 font1 = pygame.font.Font('Retro Gaming.ttf',50)
 font2 = pygame.font.Font('Retro Gaming.ttf',30)
@@ -264,7 +246,24 @@ surface = pygame.transform.scale(surface,(96,96))
 
 water_surface = pygame.Surface((1100,200))
 water_surface.fill('Lightblue')
+#Taust
 
+background11 = pygame.image.load('pildid/taust/taust1/background1.png')
+background11 = pygame.transform.scale(background11,(1000,800))
+background21 = pygame.image.load('pildid/taust/taust1/background2.png')
+background21 = pygame.transform.scale(background21,(1000,800))
+background31 = pygame.image.load('pildid/taust/taust1/background3.png')
+background31 = pygame.transform.scale(background31,(1000,800))
+bg11_width = background11.get_width()
+tiles11 = (ceil(SCREEN_WIDTH / bg11_width)) + 1
+background12 = pygame.image.load('pildid/taust/taust2/background1.png')
+background12 = pygame.transform.scale(background12,(1000,800))
+background22 = pygame.image.load('pildid/taust/taust2/background2.png')
+background22 = pygame.transform.scale(background22,(1000,800))
+background32 = pygame.image.load('pildid/taust/taust2/background3.png')
+background32 = pygame.transform.scale(background32,(1000,800))
+bg12_width = background12.get_width()
+tiles12 = (ceil(SCREEN_WIDTH / bg12_width)) + 1
 #Playeri valik
 player_stand1 = pygame.image.load('pildid/1 Pink_Monster/Pink_Monster.png')
 player_stand1 = pygame.transform.scale(player_stand1,(96,96))
@@ -294,6 +293,16 @@ def kast(x,y,x1,y1,color):
 exittext_surface = font3.render("Exit",False,"Black") #Selleks, et Exiti nupust rect argument tuleks, kirjutan selle eraldi välja
 exittext_rect = exittext_surface.get_rect(center = (550,700))
 
+#Worldmenu
+planet1_surface = pygame.image.load('pildid/Maincharacters/planet05.png')
+planet1_surface = pygame.transform.scale(planet1_surface,(320,320))
+planet1_rect = planet1_surface.get_rect(midtop = (300,400))
+
+planet2_surface = pygame.image.load('pildid/Maincharacters/planet07.png')
+planet2_surface = pygame.transform.scale(planet2_surface,(320,320))
+planet2_rect = planet2_surface.get_rect(midtop = (800,400))
+
+
 #vaenlased
 bat_fly1 = pygame.image.load('pildid/Maincharacters/bat_fly.png')
 bat_fly2 = pygame.image.load('pildid/Maincharacters/bat_hang.png')
@@ -316,15 +325,15 @@ ladybug_walk_surface = ladybug_walk[ladybug_walk_index]
 barnacle1 = pygame.image.load('pildid/Maincharacters/barnacle_bite.png')
 
 #Muutujad
-scroll = 0
-tiles1 = (ceil(SCREEN_WIDTH / bg1_width)) + 1
-tiles2 = (ceil(SCREEN_WIDTH / bg2_width)) + 1
-tiles3 = (ceil(SCREEN_WIDTH / bg3_width)) + 1
 
+
+
+scroll = 0
 coinpoint = 0
 starttime = 0
 gravitatsioon = 0
 game_active = False
+worldmenu_active = False
 
 obstacle_timer = pygame.USEREVENT + 1 #+1 vaja selleks, kuna pygame endal ka vaja evente.
 pygame.time.set_timer(obstacle_timer,1900)
@@ -335,7 +344,7 @@ pygame.time.set_timer(bat_animation_timer,200)
 ladybug_animation_timer = pygame.USEREVENT + 4
 pygame.time.set_timer(ladybug_animation_timer,1500)
 coin_animation_timer = pygame.USEREVENT + 5
-pygame.time.set_timer(coin_animation_timer,8000)
+pygame.time.set_timer(coin_animation_timer,12000)
 
 while True:
     for event in pygame.event.get():
@@ -356,49 +365,74 @@ while True:
             
         else:
             pos = pygame.mouse.get_pos()
-            if playerstand_rect1.collidepoint(pos):
-                if pygame.mouse.get_pressed() == (True,False,False):
-                    print(1)
-                    player.add(Player(1))
-                    game_active = True
-                    starttime = int(pygame.time.get_ticks()/1000)
-            if playerstand_rect2.collidepoint(pos):
-                if pygame.mouse.get_pressed() == (True,False,False):
-                    player.add(Player(2))
-                    game_active = True
-                    starttime = int(pygame.time.get_ticks()/1000)
-            if playerstand_rect3.collidepoint(pos):
-                if pygame.mouse.get_pressed() == (True,False,False):
-                    player.add(Player(3))
-                    game_active = True
-                    starttime = int(pygame.time.get_ticks()/1000)
-            if exittext_rect.collidepoint(pos):
-                if pygame.mouse.get_pressed() == (True,False,False):
-                    pygame.quit()
-                    exit()
+            if worldmenu_active:
+                if planet1_rect.collidepoint(pos):
+                    if pygame.mouse.get_pressed() == (True,False,False):
+                        worldtype = 1
+                        worldmenu_active = False
+                        game_active = True
+                        starttime = int(pygame.time.get_ticks()/1000)
+                if planet2_rect.collidepoint(pos):
+                    if pygame.mouse.get_pressed() == (True,False,False):
+                        worldtype = 2
+                        worldmenu_active = False
+                        game_active = True
+                        starttime = int(pygame.time.get_ticks()/1000)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_1:
+                        worldtype = 1
+                        worldmenu_active = False
+                        game_active = True
+                        starttime = int(pygame.time.get_ticks()/1000)
+                    elif event.key == pygame.K_2:
+                        worldtype = 2
+                        worldmenu_active = False
+                        game_active = True
+                        starttime = int(pygame.time.get_ticks()/1000)
                     
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1:
-                    player.add(Player(1))
-                    game_active = True
-                    starttime = int(pygame.time.get_ticks()/1000)
-                elif event.key == pygame.K_2:
-                    player.add(Player(2))
-                    game_active = True
-                    starttime = int(pygame.time.get_ticks()/1000)
-                elif event.key == pygame.K_3:
-                    player.add(Player(3))
-                    game_active = True
-                    starttime = int(pygame.time.get_ticks()/1000)
+
+            else:
+                if playerstand_rect1.collidepoint(pos):
+                    if pygame.mouse.get_pressed() == (True,False,False):
+                        player.add(Player(1))
+                        worldmenu_active = True
+                if playerstand_rect2.collidepoint(pos):
+                    if pygame.mouse.get_pressed() == (True,False,False):
+                        player.add(Player(2))
+                        worldmenu_active = True
+                if playerstand_rect3.collidepoint(pos):
+                    if pygame.mouse.get_pressed() == (True,False,False):
+                        player.add(Player(3))
+                        worldmenu_active = True
+                if exittext_rect.collidepoint(pos):
+                    if pygame.mouse.get_pressed() == (True,False,False):
+                        pygame.quit()
+                        exit()
+                        
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_1:
+                        player.add(Player(1))
+                        worldmenu_active = True
+                    elif event.key == pygame.K_2:
+                        player.add(Player(2))
+                        worldmenu_active = True
+                        #starttime = int(pygame.time.get_ticks()/1000)
+                    elif event.key == pygame.K_3:
+                        player.add(Player(3))
+                        worldmenu_active = True
     if game_active:
-        for i in range(0,tiles1): #Paneb pildid üksteise kõrvale, loogika on loodud youtube:Coding With Russ
-            screen.blit(background1,(i * bg1_width + scroll,0))
-            screen.blit(background2,(i * bg2_width + scroll,0))
-            screen.blit(background3,(i * bg3_width + scroll,0))
-        
+        for i in range(0,tiles11): #Paneb pildid üksteise kõrvale, loogika on loodud youtube:Coding With Russ
+            if worldtype == 1:
+                screen.blit(background11,(i * bg11_width + scroll,0))
+                screen.blit(background21,(i * bg11_width + scroll,0))
+                screen.blit(background31,(i * bg11_width + scroll,0))
+            elif worldtype == 2:
+                screen.blit(background12,(i * bg12_width + scroll,0))
+                screen.blit(background22,(i * bg12_width + scroll,0))
+                screen.blit(background32,(i * bg12_width + scroll,0))
         scroll -= 5
         #Resetib scrolli (muidu saab pilt otsa)
-        if abs(scroll) > bg1_width:
+        if abs(scroll) > bg11_width:
             scroll = 0
 
         screen.blit(water_surface,(0,600))
@@ -417,12 +451,30 @@ while True:
         game_active = obstaclecollision()
         timer()
         coinvalue()
-        
+    elif worldmenu_active:
+        screen.fill(("lightgreen"))
+        kast(700,100,200,50,"Black")
+        kast(680,80,210,60,"#93E9BE")
+        text(80,"Choose world",550,99,"Black")
+        text(45,"1",300,380,"Black")
+        text(45,"2",800,380,"Black")
+        #esimese planeedi kast
+        kast(320,320,140,400,"Black")
+        kast(300,300,150,410,"#93E9BE")
+        #teise planeedi kast
+        kast(320,320,640,400,"Black")
+        kast(300,300,650,410,"#93E9BE")
+        screen.blit(planet1_surface,planet1_rect)
+        screen.blit(planet2_surface,planet2_rect)
+        kast(150,60,475,670,"Black")
+        kast(140,50,480,675,"#93E9BE")
+        screen.blit(exittext_surface,exittext_rect)
+
     else:
         screen.fill(("lightgreen"))
         kast(96,96,368,208,"Black")
         kast(80,80,376,216,"#93E9BE")
-
+        
         kast(96,96,518,208,"Black")
         kast(80,80,526,216,"#93E9BE")
 
--- README.md
+++ README.md
@@ -2,15 +2,17 @@
 
 Mündi tekstuur   
 https://totuslotus.itch.io/pixel-coins   
-Taust   
-https://szadiart.itch.io/background-desert-mountains   
+Taust1
+https://szadiart.itch.io/background-desert-mountains
+Taust2
+https://vnitti.itch.io/glacial-mountains-parallax-background
 Alus   
 https://www.kenney.nl/assets/pixel-platformer-blocks   
 Karakterid   
 https://free-game-assets.itch.io/free-tiny-hero-sprites-pixel-art  
 Font   
 https://www.dafont.com/retro-gaming.font   
-*Maailmad   
-https://helianthus-games.itch.io/pixel-art-planets   
+*Planeedid
+https://www.kenney.nl/assets/planets
 *kirstud   
 https://merchant-shade.itch.io/16x16-mixed-rpg-icons   
